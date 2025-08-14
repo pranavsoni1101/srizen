a557149032419ae5f36d8c78bea61016127812b7
Pranav Soni <pranavsoni1101@gmail.com>
2025-08-14 20:54:15 +0530

    Added Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
+import Navbar from "@/components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,7 +35,8 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-        {children}
+          <Navbar />
+          {children}
         </ThemeProvider>
       </body>
     </html>
