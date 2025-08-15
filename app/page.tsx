0db6935dcb77da2c608e94ba3952d958f79eefb3
Pranav Soni <pranavsoni1101@gmail.com>
2025-08-15 15:39:16 +0530

    Added a container component to dynamically render content according to screen size

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Container } from "@/components/container";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 
 import { Moon, Sun } from "lucide-react"
@@ -5,8 +6,9 @@ import { useTheme } from "next-themes"
 const Home = () => {
     return(
         <>
+        <Container>
             <h1>Home</h1>
-            <ThemeSwitcher />
+        </Container>
         </>
     )
 }
