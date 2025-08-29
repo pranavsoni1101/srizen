504698cc7b71707e9066b9648d60da543cccbc09
Pranav Soni <pranavsoni1101@gmail.com>
2025-08-30 00:37:03 +0530

    Updated navbar footer and layout to use srizen

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,13 +18,13 @@ const geistMono = Geist_Mono({
 // app/layout.tsx
 export const metadata: Metadata = {
   title: {
-    default: "PixelSmith – Crafting Experiences",
-    template: "%s | PixelSmith",
+    default: "Srizen – Crafting Experiences",
+    template: "%s | Srizen",
   },
   description:
-    "PixelSmith is a creative web development studio specializing in custom websites, web apps, and digital experiences that blend design and technology.",
+    "Srizen is a creative web development studio specializing in custom websites, web apps, and digital experiences that blend design and technology.",
   keywords: [
-    "PixelSmith",
+    "Srizen",
     "web development",
     "React",
     "Next.js",
@@ -33,20 +33,20 @@ export const metadata: Metadata = {
     "frontend development",
     "backend development",
   ],
-  authors: [{ name: "PixelSmith" }],
-  metadataBase: new URL("https://builtby-pixel-smith.vercel.app"),
+  authors: [{ name: "Srizen" }],
+  metadataBase: new URL("https://srizen.com"),
   openGraph: {
-    title: "PixelSmith – Crafting Web Experiences",
+    title: "Srizen – Crafting Web Experiences",
     description:
-      "We forge digital experiences pixel by pixel. From web apps to custom solutions, PixelSmith delivers sleek, modern, and impactful products.",
+      "We forge digital experiences pixel by pixel. From web apps to custom solutions, Srizen delivers sleek, modern, and impactful products.",
     url: "https://builtby-pixel-smith.vercel.app",
-    siteName: "PixelSmith",
+    siteName: "Srizen",
     images: [
       {
         url: "/og-image.png", // make one later!
         width: 1200,
         height: 630,
-        alt: "PixelSmith – Crafting Web Experiences",
+        alt: "Srizen – Crafting Web Experiences",
       },
     ],
     locale: "en_US",
@@ -54,9 +54,9 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "PixelSmith – Crafting Web Experiences",
+    title: "Srizen – Crafting Web Experiences",
     description:
-      "PixelSmith is a creative web development studio specializing in modern, scalable, and beautifully designed digital solutions.",
+      "Srizen is a creative web development studio specializing in modern, scalable, and beautifully designed digital solutions.",
     images: ["/og-image.png"],
   },
 };
